bf2f115323725123b4264203e2179d95f0d9e489
Kalle Ruttik <kalle.ruttik@aalto.fi>
2025-04-09 18:21:41 +0300

    some cleanup

--- DECTMAC_103636_4/binMsg2unit8Converter.m
+++
@@ -1,6 +0,0 @@
-function msg = binMsg2unit8Converter(bin, lengthInBytes)
-    msg = zeros(1,lengthInBytes);
-    for i = 1:lengthInBytes
-        msg(i) = uint8(bi2de(fliplr(bin(1+(i-1)*8 : 8+(i-1)*8))));
-    end
-end
